97eeeab1c4aae440951823cab1ff086ec87d709b
Xyjfydmhfgxj <mrsydone@gmail.com>
2026-01-29 07:38:13 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -730,12 +730,10 @@ async def get_bad_files(query, file_type=None, filter=False):
 
 async def get_file_details(query):
     filter = {"file_id": query}
-    file1 = await Media1.find_one(filter)
+    file1 = ((await Media1.find_one(filter) or {}) | (await Media2.find_one(filter) or {}) |
+             (await Media3.find_one(filter) or {}) | (await Media4.find_one(filter) or {}))
     if file1:
         return [file1]
-    file2 = await Media2.find_one(filter)
-    if file2:
-        return [file2]
     return []
 
 
