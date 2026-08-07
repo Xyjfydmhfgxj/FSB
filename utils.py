fde95fb5fa693327c5b4d41d5fec5ed5bbfb6d84
Xyjfydmhfgxj <mrsydone@gmail.com>
2026-08-07 08:29:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,7 +31,7 @@ BTN_URL_REGEX = re.compile(
     r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
 )
 
-imdb = Cinemagoer() 
+imdb = None 
 TOKENS = {}
 VERIFIED = {}
 BANNED = {}
